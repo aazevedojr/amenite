b4fcd55f0445e9a786fdc357478f6c06991ca844
Elsha Kwee <elsha.e.k@gmail.com>
2017-09-28 16:20:32 -0700

    UPDATE attributes for provider model

--- db/migrate/20170928224915_create_providers.rb
+++ db/migrate/20170928224915_create_providers.rb
@@ -2,6 +2,12 @@ class CreateProviders < ActiveRecord::Migration[5.1]
   def change
     create_table :providers do |t|
       t.string :name
+      t.text :amenity_details
+      t.text :address
+      t.string :latitude
+      t.string :longitude
+      t.string :hours
+      t.string :phone
 
       t.timestamps
     end
