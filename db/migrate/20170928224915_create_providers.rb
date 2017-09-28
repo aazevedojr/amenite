458be859e9781c6a800b33df0c21f2a369a99db5
Elsha E. Kwee <elsha.e.k@gmail.com>
2017-09-28 16:06:13 -0700

    Update 20170928224915_create_providers.rb

--- db/migrate/20170928224915_create_providers.rb
+++ db/migrate/20170928224915_create_providers.rb
@@ -1,7 +1,7 @@
 class CreateProviders < ActiveRecord::Migration[5.1]
   def change
     create_table :providers do |t|
-      t.string :name=string
+      t.string :name
 
       t.timestamps
     end
